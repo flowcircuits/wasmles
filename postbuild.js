e9186a52bb3777d1b9c2abb38807b74d4ea1c139
AJ Caldwell <tomatrow@gmail.com>
2023-06-21 22:49:10 -0700

    Finagle the build system to our whims

---
+++ postbuild.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+// package the manifest
+const { version } = JSON.parse(fs.readFileSync('./package.json'))
+const package = JSON.parse(fs.readFileSync('./resources/package.json'))
+fs.writeFileSync('./pkg/package.json', JSON.stringify({
+	...package,
+	version
+}, null, 2))
+
+// package the types
+fs.writeFileSync('./pkg/types.d.ts', fs.readFileSync("./resources/types.d.ts"));
