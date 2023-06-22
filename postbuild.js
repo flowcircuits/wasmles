8e8d571bebf0fcf1f9a7003141cc2e167df041b6
AJ Caldwell <tomatrow@gmail.com>
2023-06-22 16:53:41 -0700

    Complete the incantation

--- postbuild.js
+++ postbuild.js
@@ -1,19 +1,95 @@
+const path = require("path");
 const fs = require("fs");
 
-// package the manifest
-const { version } = JSON.parse(fs.readFileSync("./package.json"));
-const package = JSON.parse(fs.readFileSync("./resources/package.json"));
-fs.writeFileSync(
-    "./pkg/package.json",
-    JSON.stringify(
-        {
-            ...package,
-            version,
-        },
-        null,
-        2
-    )
-);
+/** @param {string} str */
+function posixify(str) {
+    return str.replace(/\\/g, "/");
+}
 
-// package the types
-fs.writeFileSync("./pkg/index.d.ts", fs.readFileSync("./resources/index.d.ts"));
+/** @param {string} dir */
+function mkdirp(dir) {
+    try {
+        fs.mkdirSync(dir, { recursive: true });
+    } catch (/** @type {any} */ e) {
+        if (e.code === "EEXIST") {
+            if (!fs.statSync(dir).isDirectory()) {
+                throw new Error(
+                    `Cannot create directory ${dir}, a file already exists at this position`
+                );
+            }
+            return;
+        }
+        throw e;
+    }
+}
+
+/**
+ * @param {string} source
+ * @param {string} target
+ * @param {{
+ *   filter?: (basename: string) => boolean;
+ *   replace?: Record<string, string>;
+ * }} opts
+ */
+function copy(source, target, opts = {}) {
+    if (!fs.existsSync(source)) return [];
+
+    /** @type {string[]} */
+    const files = [];
+
+    const prefix = posixify(target) + "/";
+
+    const regex = opts.replace
+        ? new RegExp(`\\b(${Object.keys(opts.replace).join("|")})\\b`, "g")
+        : null;
+
+    /**
+     * @param {string} from
+     * @param {string} to
+     */
+    function go(from, to) {
+        if (opts.filter && !opts.filter(path.basename(from))) return;
+
+        const stats = fs.statSync(from);
+
+        if (stats.isDirectory()) {
+            fs.readdirSync(from).forEach((file) => {
+                go(path.join(from, file), path.join(to, file));
+            });
+        } else {
+            mkdirp(path.dirname(to));
+
+            if (opts.replace) {
+                const data = fs.readFileSync(from, "utf-8");
+                fs.writeFileSync(
+                    to,
+                    data.replace(
+                        /** @type {RegExp} */ (regex),
+                        (_match, key) =>
+                            /** @type {Record<string, string>} */ (
+                                opts.replace
+                            )[key]
+                    )
+                );
+            } else {
+                fs.copyFileSync(from, to);
+            }
+
+            files.push(
+                to === target
+                    ? posixify(path.basename(to))
+                    : posixify(to).replace(prefix, "")
+            );
+        }
+    }
+
+    go(source, target);
+
+    return files;
+}
+
+copy("./resources", "./pkg", {
+    replace: {
+        VERSION: JSON.parse(fs.readFileSync("./package.json")).version + "",
+    },
+});
---
+++ resources/browser/index.d.ts
@@ -0,0 +1,5 @@
+export * from "../functions";
+
+export function initSync(): void;
+
+export default function init(): Promise<void>;
