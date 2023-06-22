01b6ffde0ac92100d91c31418a87d647828def57
Andrew Krippner <andrewkrip@gmail.com>
2023-06-21 23:03:44 -0700

    prettier

--- postbuild.js
+++ postbuild.js
@@ -1,12 +1,19 @@
-const fs = require('fs')
+const fs = require("fs");
 
 // package the manifest
-const { version } = JSON.parse(fs.readFileSync('./package.json'))
-const package = JSON.parse(fs.readFileSync('./resources/package.json'))
-fs.writeFileSync('./pkg/package.json', JSON.stringify({
-	...package,
-	version
-}, null, 2))
+const { version } = JSON.parse(fs.readFileSync("./package.json"));
+const package = JSON.parse(fs.readFileSync("./resources/package.json"));
+fs.writeFileSync(
+    "./pkg/package.json",
+    JSON.stringify(
+        {
+            ...package,
+            version,
+        },
+        null,
+        2
+    )
+);
 
 // package the types
-fs.writeFileSync('./pkg/types.d.ts', fs.readFileSync("./resources/types.d.ts"));
+fs.writeFileSync("./pkg/types.d.ts", fs.readFileSync("./resources/types.d.ts"));
